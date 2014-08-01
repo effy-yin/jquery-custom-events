bcccfc708634f94fdb9e4ad90059552f7dccded0
dodo糯 <effy.yin@gmail.com>
2014-08-01 15:05:55 +0800

    add jquery.input.js

---
+++ jquery.input.js
@@ -0,0 +1,48 @@
+/*
+* jQuery input event
+* 調用： $(elem).bind('input', function () {});
+*/
+(function ($) {
+
+    // IE6\7\8不支持input事件，但支持propertychange事件
+    if ('onpropertychange' in document) {
+        // 检查是否为可输入元素
+        var rInput = /^INPUT|TEXTAREA$/,
+            isInput = function (elem) {
+                return rInput.test(elem.nodeName);
+            };
+                       
+        $.event.special.input = {
+            setup: function () {
+                var elem = this;
+                if (!isInput(elem)) 
+                    return false;
+                
+                $.data(elem, '@oldValue', elem.value);
+                $.event.add(elem, 'propertychange', function (event) {
+                    // 元素属性任何变化都会触发propertychange事件
+                    // 需要屏蔽掉非value的改变，以便接近标准的onput事件
+                    if ($.data(this, '@oldValue') !== this.value) {
+                        $.event.trigger('input', null, this);
+                    };
+                    
+                    $.data(this, '@oldValue', this.value);
+                });
+            },
+            teardown: function () {
+                var elem = this;
+                if (!isInput(elem)) 
+                    return false;
+                
+                $.event.remove(elem, 'propertychange');
+                $.removeData(elem, '@oldValue');
+            }
+        };
+    };
+
+    // 声明快捷方式：$(elem).input(function () {});
+    $.fn.input = function (callback) {
+        return this.bind('input', callback);
+    };
+
+})(jQuery);
